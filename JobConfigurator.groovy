fbbf856f7d0d139a7ca7dc81af575bcdd460aea5
Mark Heiges <mheiges@uga.edu>
2015-03-20 12:24:46 -0400

    maybe pass jobname to featureDescription without breaking all the things

--- JobConfigurator.groovy
+++ JobConfigurator.groovy
@@ -40,7 +40,7 @@ public class JobConfigurator {
           def svnDefaultLocations = Values.svnDefaultLocations
           map[jobName] = [
             label : hostconf['label'],
-            description : hostconf['description'] ?: Values.stdDescription(jobName, "boo"),
+            description : hostconf['description'](jobName, "boo") ?: Values.stdDescription(jobName, "boo"),
             logRotator : hostconf['logRotator'] ?: [7, -1, -1, -1],
             disabled : existingJob ? existingJob.disabled : false,
             quietPeriod : hostconf['quietPeriod'] ?: null,
