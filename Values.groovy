56342f7b3cb1187086aebf5a7af4595bdc977605
Mustafa Nural <mvnural@gmail.com>
2026-02-16 16:42:28 -0500

    fix sitesearch update step.
    remove ipv6 disable now that we use --network=host

--- Values.groovy
+++ Values.groovy
@@ -326,7 +326,6 @@ Sitesearch step
             ## At some point remove the TNS_ADMIN env and ldap mount for Oracle. 
             ## They are not needed for postgres sites but they would be needed if we move legacy sites to new servers. 
             podman run --rm \
-              --sysctl net.ipv6.conf.all.disable_ipv6=1 \\
               --network=host \\
               --env TNS_ADMIN=/jdbc/network/admin \\
               --env-file=/var/www/${host}.${sld}.${tld}/gus_home/config/${model}/container_env \\
