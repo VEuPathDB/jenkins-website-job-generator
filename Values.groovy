537c382304e32060e3c06ab0fd0404a8f4befbb4
Mustafa Nural <mvnural@gmail.com>
2026-02-19 12:09:46 -0500

    fix typo

--- Values.groovy
+++ Values.groovy
@@ -326,7 +326,7 @@ Sitesearch step
           
           ## At some point remove the TNS_ADMIN env and ldap mount for Oracle. 
           ## They are not needed for postgres sites but they would be needed if we move legacy sites to new servers. 
-          podman run --rm \
+          podman run --rm \\
             --network=host \\
             --env TNS_ADMIN=/jdbc/network/admin \\
             --env-file=/var/www/${host}.${sld}.${tld}/gus_home/config/${model}/container_env ${cohortOverride} \\
